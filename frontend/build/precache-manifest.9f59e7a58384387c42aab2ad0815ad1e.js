6266777ef149754af1b0084211bfa1378f61752d
Anup <anuppoudel60@gmail.com>
2022-12-08 14:42:52 +1100

    build file added

---
+++ frontend/build/precache-manifest.9f59e7a58384387c42aab2ad0815ad1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "86d17481a896eb29218f",
+    "url": "/static/css/main.fa0fd08c.chunk.css"
+  },
+  {
+    "revision": "86d17481a896eb29218f",
+    "url": "/static/js/main.86d17481.chunk.js"
+  },
+  {
+    "revision": "632cbe02aa595d27f7d9",
+    "url": "/static/css/1.86fd67b4.chunk.css"
+  },
+  {
+    "revision": "632cbe02aa595d27f7d9",
+    "url": "/static/js/1.632cbe02.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0fc33b6c142a26ddc84053e5df7f1827",
+    "url": "/static/media/aaa.0fc33b6c.jpg"
+  },
+  {
+    "revision": "a5369809569edc4e18ad64c495326f6e",
+    "url": "/static/media/bbb.a5369809.jpg"
+  },
+  {
+    "revision": "782adc2b6062ab00461359da5b02b753",
+    "url": "/static/media/Profile.782adc2b.png"
+  },
+  {
+    "revision": "9e10657bff9facfb5e17fec1155a54f1",
+    "url": "/index.html"
+  }
+];
